d100b96e78d9f663df0d78668640e8b13e9e02ad
Haozhou Wang <hawang@pivotal.io>
2018-09-10 18:41:36 +0800

    working with catalog stage 5

--- src/test/regress/sql/disk_quota.sql
+++ src/test/regress/sql/disk_quota.sql
@@ -10,8 +10,8 @@ CREATE SCHEMA test_ns_disk_quota
        );
 
 CREATE TABLE quota_2 (
-	a 			int2,
-	b			float4
+        a                       int2,
+        b                       float4
 );
 
 CREATE ROLE test_user_quota_1;
@@ -20,7 +20,7 @@ CREATE DISK QUOTA abc ON TABLE quota_2 WITH ();
 
 CREATE DISK QUOTA abc ON TABLE quota_2 WITH (abc='100M');
 
-CREATE DISK QUOTA abc ON TABLE quota_2 WITH (quota='100M', 'abc'='100M');
+CREATE DISK QUOTA abc ON TABLE quota_2 WITH (quota='100M', abc='100M');
 
 CREATE DISK QUOTA abc ON TABLE quota_2 WITH (quota='100M', quota='200M');
 
@@ -38,10 +38,16 @@ CREATE DISK QUOTA abc ON SCHEMA test_ns_disk_quota WITH (quota='100M');
 
 CREATE DISK QUOTA abc_1 ON SCHEMA test_ns_disk_quota WITH (quota='100M');
 
-CREATE DISK QUOTA abc_1 ON TABLE test_ns_disk_quota.quota_1 WITH (quota='100M');
+CREATE DISK QUOTA abc_2 ON TABLE test_ns_disk_quota.quota_1 WITH (quota='100M');
 
-CREATE DISK QUOTA abc_2 ON USER test_user_quota_1 WITH (quota='100M');
+CREATE DISK QUOTA abc_3 ON USER test_user_quota_1 WITH (quota='100M');
 
+
+DROP DISK QUOTA abc;
+DROP DISK QUOTA abc_1;
+DROP DISK QUOTA abc_2;
+DROP DISK QUOTA abc_3;
+DROP DISK QUOTA abc_no;
 
 DROP SCHEMA test_ns_disk_quota CASCADE;
 DROP TABLE quota_2;
