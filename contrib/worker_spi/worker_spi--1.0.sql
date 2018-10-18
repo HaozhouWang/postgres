bbc3dcc33cfdc6c0f89f750863dec0478a0a87c0
Hubert Zhang <hubertzhang@apache.org>
2018-10-18 16:01:17 +0800

    Merge 2

--- contrib/worker_spi/worker_spi--1.0.sql
+++ contrib/worker_spi/worker_spi--1.0.sql
@@ -3,12 +3,7 @@
 -- complain if script is sourced in psql, rather than via CREATE EXTENSION
 \echo Use "CREATE EXTENSION worker_spi" to load this file. \quit
 
-CREATE FUNCTION worker_spi_launch(pg_catalog.int4)
-RETURNS pg_catalog.int4 STRICT
-AS 'MODULE_PATHNAME'
-LANGUAGE C;
-
-CREATE FUNCTION set_disk_quota_limit(pg_catalog.int4,pg_catalog.int4)
+CREATE FUNCTION set_disk_quota_limit(int4,int8)
 RETURNS void STRICT
 AS 'MODULE_PATHNAME'
 LANGUAGE C;
@@ -18,7 +13,7 @@ CREATE SCHEMA quota;
 set search_path='quota';
 
 -- Configuration table
-create table quota.config (targetOid oid PRIMARY key, quota int8);
+create table quota.config (targetOid oid PRIMARY key, quotalimitMB int8);
 
 SELECT pg_catalog.pg_extension_config_dump('quota.config', '');
 
