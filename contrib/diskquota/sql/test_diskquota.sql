8d43c6149608e9da3cb9b9d5a9df21380f67c483
Hubert Zhang <hubertzhang@apache.org>
2018-10-31 03:34:26 +0000

    Add partition table test.

--- contrib/diskquota/sql/test_diskquota.sql
+++ contrib/diskquota/sql/test_diskquota.sql
@@ -98,7 +98,7 @@ INSERT INTO a5
 SELECT (SELECT 
         string_agg(chr(floor(random() * 26)::int + 65), '')
         FROM generate_series(1,100000)) 
-FROM generate_series(1,1000);
+FROM generate_series(1,1000000);
 reset search_path;
 
 -- Test vacuum full
@@ -132,6 +132,29 @@ select pg_sleep(5);
 insert into a7 select generate_series(1,30);
 reset search_path;
 
--- Test SPI
+-- Test partition table
+create schema s8;
+select diskquota.set_schema_quota('s8', '1 MB');
+set search_path to s8;
+CREATE TABLE measurement (
+    city_id         int not null,
+    logdate         date not null,
+    peaktemp        int,
+    unitsales       int
+)PARTITION BY RANGE (logdate);
+CREATE TABLE measurement_y2006m02 PARTITION OF measurement
+    FOR VALUES FROM ('2006-02-01') TO ('2006-03-01');
+
+CREATE TABLE measurement_y2006m03 PARTITION OF measurement
+    FOR VALUES FROM ('2006-03-01') TO ('2006-04-01');
+insert into measurement select generate_series(1,15000), '2006-02-01' ,1,1;
+select pg_sleep(5);
+insert into measurement select 1, '2006-02-01' ,1,1;
+-- expect insert fail
+insert into measurement select generate_series(1,100000000), '2006-02-01' ,1,1;
+-- expect insert fail
+insert into measurement select 1, '2006-02-01' ,1,1;
+reset search_path;
+
 drop extension diskquota;
 
